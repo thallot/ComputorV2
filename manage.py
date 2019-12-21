540e085e0a5a9b7bba075ee566f88669da4c7340
thallot <tom.2306@hotmail.fr>
2019-12-21 15:11:50 +0100

    Fix assignation

--- manage.py
+++ manage.py
@@ -29,16 +29,19 @@ def assignFunction(ParseOne, ParseTwo, var, fun):
     return fun
 
 def assignVar(ParseOne, ParseTwo, var, fun):
-    res, error = evaluate(ParseTwo.list, var, fun)
-    if error:
+    try:
+        res, error = evaluate(ParseTwo.list, var, fun)
+        if error:
+            print('\033[31mInvalid assignement\033[37m')
+        elif not res == None:
+            for key in fun:
+                if fun[key].var == ParseOne.list[0].value:
+                    print('\033[31mWarning : [', ParseOne.list[0].value, '] is a variable of function [', \
+                    fun[key], ']\nThis can cause unexpected behavior\033[37m')
+            var[ParseOne.list[0].value] = res
+            print('\033[32mAssignation:', ParseOne.list[0].value , ' = ', res, '\033[37m')
+    except:
         print('\033[31mInvalid assignement\033[37m')
-    elif not res == None:
-        for key in fun:
-            if fun[key].var == ParseOne.list[0].value:
-                print('\033[31mWarning : [', ParseOne.list[0].value, '] is a variable of function [', \
-                fun[key], ']\nThis can cause unexpected behavior\033[37m')
-        var[ParseOne.list[0].value] = res
-        print('\033[32mAssignation:', ParseOne.list[0].value , ' = ', res, '\033[37m')
     return var
 
 def printFunction(Parsing, var, fun):
